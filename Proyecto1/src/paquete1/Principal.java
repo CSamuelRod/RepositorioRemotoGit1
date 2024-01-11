868e5c909f0a6ded81262c599b86a0a65f0faac6
Diurno <Diurno@DESKTOP-A2FPPFB>
2024-01-11 12:17:07 +0100

    Segundo commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ejemplo de control de versiones Git");
+		System.out.println("Segundo ejemplo de control de versiones Git");
 	}
 
 }
